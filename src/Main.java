615d89e02cd3d9494748ccc73c6a3d61157395a3
Cranky Bawler <6666lenia6666@mail.ru>
2023-01-25 19:22:19 +0300

    Переменные часть 2 master

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+    }
+    public static void task1 () {
+        System.out.println("Задача 1");
+        // write your code here
+        int one = 4648355;
+        System.out.println("Значение переменной one с типом int равно " +one);
+        byte two = 123;
+        System.out.println("Значение переменной two с типом byte равно " +two);
+        short three = 31382;
+        System.out.println("Значение переменной three с типом short равно " +three);
+        long four = 318546;
+        System.out.println("Значение переменной four с типом long равно " + four);
+        float five = (float) 5.684;
+        System.out.println("Значение переменной five с типом float равно " + five);
+        double six = 5.478653148;
+        System.out.println("Значение переменной six с типом duoble равно " + six);
+            }
+}
